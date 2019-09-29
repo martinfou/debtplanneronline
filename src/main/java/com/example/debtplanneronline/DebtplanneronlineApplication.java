7eab29a67b0209dc3346b7efe8fc3ec66d9b0058
admin <admin@DESKTOP-UL2HGL5>
2019-09-29 01:30:39 -0400

    assing basic spring structure

---
+++ src/main/java/com/example/debtplanneronline/DebtplanneronlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.debtplanneronline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtplanneronlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtplanneronlineApplication.class, args);
+	}
+
+}
